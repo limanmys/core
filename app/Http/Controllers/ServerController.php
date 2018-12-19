42a0c57dfec10e94d4a19b7d41c5badcc4dfb1e1
mertcelen <mcelen94@gmail.com>
2018-12-19 16:52:23 +0300

    Daily update.

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -13,67 +13,66 @@ use Illuminate\Support\Facades\Auth;
 class ServerController extends Controller
 {
     public static $protected = true;
-    
-    public function index(){
-        $permissions = request('permissions');
-        $servers = [];
-        if($permissions->__get("server") == null){
-            return view('server.index');
-        }
-        foreach ($permissions->server as $server_id) {
-            if(Server::where('_id',$server_id)->exists()){
-                array_push($servers,Server::where('_id',$server_id)->first());    
-            }
-        }
-        return view('server.index',[
+
+    public function index()
+    {
+        // Retrieve all servers.
+        $servers = Server::getAll();
+        // Filters servers for permissions.
+//        $servers = Server::filterPermissions($servers);
+
+        return view('server.index', [
             "servers" => $servers
         ]);
     }
 
-    public function add(Request $request){
+    public function add(Request $request)
+    {
         $data = $request->all();
         $server = new Server($data);
         $server->user_id = Auth::id();
         $server->extensions = [];
         $server->save();
         $output = Key::init(request('username'), request('password'), request('ip_address'),
-            request('port'),Auth::id());
+            request('port'), Auth::id());
         $key = new Key($data);
         $key->server_id = $server->id;
         $key->user_id = Auth::id();
         $permissions = $request->get('permissions');
         $user_servers = $permissions->server;
-        array_push($user_servers,$server->_id);
+        array_push($user_servers, $server->_id);
         $permissions->server = $user_servers;
         $permissions->save();
         $key->save();
-        return response(route('server_one',$server->id),200);
+        return response(route('server_one', $server->id), 200);
     }
 
-    public function remove(){
-        Server::where('_id',\request('server_id'))->delete();
+    public function remove()
+    {
+        Server::where('_id', \request('server_id'))->delete();
         Key::where('server_id', \request('server_id'))->delete();
-        $user_permissions = Permission::where('server','like',request('server_id'))->get();
+        $user_permissions = Permission::where('server', 'like', request('server_id'))->get();
         foreach ($user_permissions as $permission) {
             $servers = $permission->server;
-            unset($servers[array_search('server_id',$servers)]);
+            unset($servers[array_search('server_id', $servers)]);
             $permission->server = $servers;
-            $permission->save();    
+            $permission->save();
         }
         return route('servers');
     }
 
-    public function one(){
-        $scripts = Script::where('extensions','server')->get();
+    public function one()
+    {
+        $scripts = Script::where('extensions', 'server')->get();
         $server = \request('server');
         $services = $server->extensions;
-        for ($i = 0 ; $i < count($services); $i++){
-            if($services[$i] == "kullanıcılar" || $services[$i] == "gruplar"){
+        for ($i = 0; $i < count($services); $i++) {
+            if ($services[$i] == "kullanıcılar" || $services[$i] == "gruplar") {
                 unset($services[$i]);
-                array_push($services,'ldap');
+                array_push($services, 'ldap');
             }
         }
-        return view('server.one',[
+        return view('server.one', [
             "stats" => \request('server')->run("df -h"),
             "hostname" => request('server')->run("hostname"),
             "server" => \request('server'),
@@ -82,33 +81,36 @@ class ServerController extends Controller
         ]);
     }
 
-    public function run(){
-        $output = Server::where('_id',\request('server_id'))->first()->run(\request('command'));
+    public function run()
+    {
+        $output = Server::where('_id', \request('server_id'))->first()->run(\request('command'));
         return $output;
     }
 
-    public function runScript(){
-        $script = Script::where('_id',\request('script_id'))->first();
-        $inputs = explode(',',$script->inputs);
+    public function runScript()
+    {
+        $script = Script::where('_id', \request('script_id'))->first();
+        $inputs = explode(',', $script->inputs);
         $params = "";
-        foreach ($inputs as $input){
-            $params = $params. " " . \request(explode(':',$input)[0]);
+        foreach ($inputs as $input) {
+            $params = $params . " " . \request(explode(':', $input)[0]);
         }
-        $output = Server::where('_id',\request('server_id'))->first()->runScript($script, $params);
+        $output = Server::where('_id', \request('server_id'))->first()->runScript($script, $params);
         return [
             "result" => 200,
             "data" => $output
         ];
     }
 
-    public function check(){
-        $extension = Extension::where('name','like',request('extension'))->first();
-        $output = Server::where('_id',\request('server_id'))->first()->isRunning($extension->service);
-        if($output == "active\n"){
+    public function check()
+    {
+        $extension = Extension::where('name', 'like', request('extension'))->first();
+        $output = Server::where('_id', \request('server_id'))->first()->isRunning($extension->service);
+        if ($output == "active\n") {
             $result = 200;
-        }else if($output === "inactive\n"){
+        } else if ($output === "inactive\n") {
             $result = 202;
-        }else{
+        } else {
             $result = 201;
         }
         return [
@@ -117,13 +119,14 @@ class ServerController extends Controller
         ];
     }
 
-    public function network(){
+    public function network()
+    {
         $server = \request('server');
         $parameters = \request('ip') . ' ' . \request('cidr') . ' ' . \request('gateway') . ' ' . \request('interface');
-        $server->systemScript('network',$parameters);
+        $server->systemScript('network', $parameters);
         sleep(3);
-        $output = shell_exec("echo exit | telnet " . \request('ip') ." " . $server->port);
-        if (strpos($output,"Connected to " . \request('ip')) == false){
+        $output = shell_exec("echo exit | telnet " . \request('ip') . " " . $server->port);
+        if (strpos($output, "Connected to " . \request('ip')) == false) {
             return [
                 "result" => 201,
                 "data" => $output
@@ -133,30 +136,32 @@ class ServerController extends Controller
             'ip_address' => \request('ip')
         ]);
         Key::init($server->key["username"], request('password'), \request('ip'),
-            $server->port,Auth::id());
+            $server->port, Auth::id());
         return [
             "result" => 200,
             "data" => $output
         ];
     }
 
-    public function hostname(){
+    public function hostname()
+    {
         $server = \request('server');
-        $output = $server->systemScript('hostname',\request('hostname'));
+        $output = $server->systemScript('hostname', \request('hostname'));
         return [
             "result" => 200,
             "data" => $output
         ];
     }
 
-    public function isAlive(){
-        $output = shell_exec("echo exit | telnet " . \request('ip') ." " . \request('port'));
-        if (strpos($output,"Connected to " . \request('ip')) == false){
+    public function isAlive()
+    {
+        $output = shell_exec("echo exit | telnet " . \request('ip') . " " . \request('port'));
+        if (strpos($output, "Connected to " . \request('ip')) == false) {
             return [
                 "result" => 201,
                 "data" => $output
             ];
-        }else{
+        } else {
             return [
                 "result" => 200,
                 "data" => $output
@@ -164,29 +169,31 @@ class ServerController extends Controller
         }
     }
 
-    public function service(){
+    public function service()
+    {
         $server = \request('server');
-        $service = Extension::where('name','like',\request('extension'))->first()->service;
+        $service = Extension::where('name', 'like', \request('extension'))->first()->service;
         $output = $server->run("sudo systemctl " . \request('action') . ' ' . $service);
-        return[
+        return [
             "result" => 200,
             "data" => $output
         ];
     }
 
-    public function enableExtension(){
-        $extension = Extension::where('_id',\request('extension_id'))->first();
-        $script = Script::where('unique_code',$extension->setup)->first();
+    public function enableExtension()
+    {
+        $extension = Extension::where('_id', \request('extension_id'))->first();
+        $script = Script::where('unique_code', $extension->setup)->first();
         $server = \request('server');
-        $output = $server->runScript($script,\request('domain') . " " . \request('interface'));
-        if($server->isRunning($extension->service) == "active\n"){
+        $output = $server->runScript($script, \request('domain') . " " . \request('interface'));
+        if ($server->isRunning($extension->service) == "active\n") {
             $server->extensions = array_merge($server->extensions, [\request('extension')]);
             $server->save();
             return [
                 "result" => 200,
                 "data" => $output
             ];
-        }else{
+        } else {
             return [
                 "result" => 201,
                 "data" => $output
@@ -194,7 +201,8 @@ class ServerController extends Controller
         }
     }
 
-    public function update(){
+    public function update()
+    {
         $output = request('server')->update([
             "name" => request('name')
         ]);
--- routes/web.php
+++ routes/web.php
@@ -3,146 +3,153 @@
 Auth::routes();
 
 // Change the language
-
 Route::post('/locale', 'HomeController@setLocale')->name('set_locale');
+
+Route::group(['middleware' => ['auth','permissions']],function () {
 
 // Home Route
 
-Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/', 'HomeController@index')->name('home');
 
 // Servers Route
 
-Route::get('/sunucular', 'ServerController@index')->name('servers');
+    Route::get('/sunucular', 'ServerController@index')->name('servers');
 
 // Add Server Route
 
-Route::post('/sunucu/ekle', 'ServerController@add')->name('server_add')->middleware('parameters:username,password,ip_address,port');
+    Route::post('/sunucu/ekle', 'ServerController@add')->name('server_add')->middleware('parameters:username,password,ip_address,port');
 
 // Server Status Route (Telnet)
 
-Route::post('/api/status', 'ServerController@isAlive')->middleware('parameters:ip,port');
+    Route::post('/api/status', 'ServerController@isAlive')->middleware('parameters:ip,port');
 
 // Middleware to convert server_id to server object.
 
-Route::group(['middleware' => ['server']],function (){
+    Route::group(['middleware' => ['server']], function () {
 
-    // Extension' Server' Home Route
+        // Extension' Server' Home Route
 
-    Route::get('/l/{extension_id}/{city}/{server_id}', 'ExtensionController@server')->name('extension_server');
+        Route::get('/l/{extension_id}/{city}/{server_id}', 'ExtensionController@server')->name('extension_server');
 
-    // Extension' Server' Any Route Handler
+        // Extension' Server' Any Route Handler
 
-    Route::get('/l/{extension_id}/{city}/{server_id}/{unique_code}', 'ExtensionController@route')->middleware('script_parameters');
+        Route::get('/l/{extension_id}/{city}/{server_id}/{unique_code}', 'ExtensionController@route')->middleware('script_parameters');
 
-    // Single Server Details Route
+        // Single Server Details Route
 
-    Route::get('/sunucular/{server_id}', 'ServerController@one')->name('server_one');
+        Route::get('/sunucular/{server_id}', 'ServerController@one')->name('server_one');
 
-    // Remove Server Route
+        // Remove Server Route
 
-    Route::post('/sunucu/sil', 'ServerController@remove')->name('server_remove')->middleware('parameters:server_id');
+        Route::post('/sunucu/sil', 'ServerController@remove')->name('server_remove')->middleware('parameters:server_id');
 
-    // Server Update Route
+        // Server Update Route
 
-    Route::post('/sunucu/guncelle', 'ServerController@update')->name('server_update')->middleware('parameters:server_id,name');
+        Route::post('/sunucu/guncelle', 'ServerController@update')->name('server_update')->middleware('parameters:server_id,name');
 
-    // Server Command Route
+        // Server Command Route
 
-    Route::post('/sunucu/calistir', 'ServerController@run')->name('server_run');
+        Route::post('/sunucu/calistir', 'ServerController@run')->name('server_run');
 
-    // Server' Service Status Route
+        // Server' Service Status Route
 
-    Route::post('/sunucu/kontrol', 'ServerController@check')->name('server_check')->middleware('parameters:extension_id,server_id');
+        Route::post('/sunucu/kontrol', 'ServerController@check')->name('server_check')->middleware('parameters:extension_id,server_id');
 
-    // Server Network Update
+        // Server Network Update
 
-    Route::post('/sunucu/network', 'ServerController@network')->name('server_network')->middleware('parameters:ip,cidr,gateway,interface,password');
+        Route::post('/sunucu/network', 'ServerController@network')->name('server_network')->middleware('parameters:ip,cidr,gateway,interface,password');
 
-    // Server Hostname Update
+        // Server Hostname Update
 
-    Route::post('/sunucu/hostname', 'ServerController@hostname')->name('server_hostname')->middleware('parameters:hostname');
+        Route::post('/sunucu/hostname', 'ServerController@hostname')->name('server_hostname')->middleware('parameters:hostname');
 
-    // Server Service Run,Stop,Enable,Disable Route
+        // Server Service Run,Stop,Enable,Disable Route
 
-    Route::post('/sunucu/servis', 'ServerController@service')->name('server_service')->middleware('parameters:extension_id,action');
+        Route::post('/sunucu/servis', 'ServerController@service')->name('server_service')->middleware('parameters:extension_id,action');
 
-    // Server Extension Installation Route
+        // Server Extension Installation Route
 
-    Route::post('/sunucu/eklenti', 'ServerController@enableExtension')->name('server_extension');
+        Route::post('/sunucu/eklenti', 'ServerController@enableExtension')->name('server_extension');
     });
 
 // SSH Key List Route
 
-Route::get('/anahtarlar', 'SshController@index')->name('keys');
+    Route::get('/anahtarlar', 'SshController@index')->name('keys');
 
 // SSH Key Add Route
 
-Route::post('/anahtar/ekle', 'SshController@add')->name('key_add');
+    Route::post('/anahtar/ekle', 'SshController@add')->name('key_add');
 
 // User Details Route
 
-Route::get('/kullanici/{user_id}', 'UserController@one')->name('user');
+    Route::get('/kullanici/{user_id}', 'UserController@one')->name('user');
 
 // Script List Route
 
-Route::get('/betikler', 'ScriptController@index')->name('scripts');
+    Route::get('/betikler', 'ScriptController@index')->name('scripts');
 
 // Script Add View Route
 
-Route::get('/betik/ekle', 'ScriptController@add')->name('script_add');
+    Route::get('/betik/ekle', 'ScriptController@add')->name('script_add');
 
 // Script Add Route
 
-Route::post('/betik/ekle', 'ScriptController@create')->name('script_create');
+    Route::post('/betik/ekle', 'ScriptController@create')->name('script_create');
 
 // Script Details Route
 
-Route::get('/betik/{script_id}', 'ScriptController@one')->name('script_one');
+    Route::get('/betik/{script_id}', 'ScriptController@one')->name('script_one');
 
 // Script Run Route
 
-Route::post('/betik/calistir', 'ServerController@runScript')->name('script_run');
+    Route::post('/betik/calistir', 'ServerController@runScript')->name('script_run');
 
 // Script Upload Route
 
-Route::post('/betik/yukle', 'ScriptController@upload')->name('script_upload');
+    Route::post('/betik/yukle', 'ScriptController@upload')->name('script_upload');
 
 // Extension Page (City Select) Route
 
-Route::get('/l/{extension_id}', 'ExtensionController@index')->name('extension_id');
+    Route::get('/l/{extension_id}', 'ExtensionController@index')->name('extension_id');
 
 // Extension City Servers Route
 
-Route::get('/l/{extension_id}/{city}', 'ExtensionController@city')->name('extension_city');
+    Route::get('/l/{extension_id}/{city}', 'ExtensionController@city')->name('extension_city');
 
 // Settings Route
 
-Route::get('/ayarlar', 'SettingsController@index')->name('settings');
+    Route::get('/ayarlar', 'SettingsController@index')->name('settings');
 
 // Extensions List Route
 
-Route::get('/eklentiler', 'ExtensionController@settings')->name('extensions_settings');
+    Route::get('/eklentiler', 'ExtensionController@settings')->name('extensions_settings');
 
 // Extension Details Route
 
-Route::get('/eklentiler/{extension_id}', 'ExtensionController@one')->name('extension_one');
+    Route::get('/eklentiler/{extension_id}', 'ExtensionController@one')->name('extension_one');
 
 // Extension Management Route
 
-Route::post('/extension/{unique_code}', 'ExtensionController@route')->name('extension_api')->middleware(['server','script_parameters']);
+    Route::post('/extension/{unique_code}', 'ExtensionController@route')->name('extension_api')->middleware(['server', 'script_parameters']);
 
 // My Requests Route
 
-Route::get('/taleplerim', 'HomeController@all')->name('request_permission');
+    Route::get('/taleplerim', 'HomeController@all')->name('request_permission');
 
 // Send Request Route
 
-Route::post('/talep', 'HomeController@request')->name('request_send');
+    Route::post('/talep', 'HomeController@request')->name('request_send');
 
 // List All Requests Route
 
-Route::get('/talepler', 'PermissionController@all')->name('request_list');
+    Route::get('/talepler', 'PermissionController@all')->name('request_list');
 
 // Request Details Page
 
-Route::get('/talep/{request_id}', 'PermissionController@one')->name('request_one');
+    Route::get('/talep/{request_id}', 'PermissionController@one')->name('request_one');
+
+
+// Search Page
+
+    Route::post('/arama/','SearchController@index')->name('search');
+});
