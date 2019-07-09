42745f4b42d9d9955b1eefa2245d1cb0c02da48e
brnskn <baransekin@gmail.com>
2019-07-09 16:56:29 +0300

    Bug fix

--- resources/views/extension_pages/server.blade.php
+++ resources/views/extension_pages/server.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="{{route('home')}}">{{__("Ana Sayfa")}}</a></li>
-        <li class="breadcrumb-item"><a href="/l/{{extension()->id}}">{{extension()->name}} {{ __('Sunucuları') }}</a>
-        </li>
-        <li class="breadcrumb-item"><a href="/l/{{extension()->id}}/{{request('city')}}">{{cities(request('city'))}}</a></li>
-        <li class="breadcrumb-item active" aria-current="page">{{server()->name}}</li>
-    </ol>
-    <div class="right" style="float:right;margin-top:-55px">
-            <button class="btn btn-primary" onclick="location.href = '{{route('extension_server_settings_page',[
-                "server_id" => server()->id,
-                "extension_id" => extension()->id
-            ])}}'"><i class="fa fa-cogs"></i></button>
-            <button class="btn btn-primary" onclick="location.href = '{{route('server_one',[
-                "server_id" => server()->id,
-            ])}}'"><i class="fa fa-server"></i></button>
+  <div class="row">
+    <div class="col-md-12">
+      <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="{{route('home')}}">{{__("Ana Sayfa")}}</a></li>
+          <li class="breadcrumb-item"><a href="/l/{{extension()->id}}">{{extension()->name}} {{ __('Sunucuları') }}</a>
+          </li>
+          <li class="breadcrumb-item"><a href="/l/{{extension()->id}}/{{request('city')}}">{{cities(request('city'))}}</a></li>
+          <li class="breadcrumb-item active" aria-current="page">{{server()->name}}</li>
+      </ol>
+      <div class="right" style="float:right;margin-top:-55px">
+              <button class="btn btn-primary" onclick="location.href = '{{route('extension_server_settings_page',[
+                  "server_id" => server()->id,
+                  "extension_id" => extension()->id
+              ])}}'"><i class="fa fa-cogs"></i></button>
+              <button class="btn btn-primary" onclick="location.href = '{{route('server_one',[
+                  "server_id" => server()->id,
+              ])}}'"><i class="fa fa-server"></i></button>
+      </div>
+      <div class="card">
+          <div class="card-body mainArea">{!!$view!!}</div>
+      </div>
     </div>
-    <div class="card">
-        <div class="card-body mainArea">{!!$view!!}</div>
-    </div>
+  </div>
 @endsection
