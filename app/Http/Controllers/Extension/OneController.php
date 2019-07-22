9b5b2fc24aec98cafa49be7e09a85ca6a8848627
mertcelen <mcelen94@gmail.com>
2019-07-22 08:24:05 +0300

    Support for #26
    Signed-off-by: mertcelen <mcelen94@gmail.com>

--- app/Http/Controllers/Extension/OneController.php
+++ app/Http/Controllers/Extension/OneController.php
@@ -621,10 +621,16 @@ class OneController extends Controller
 
         $token = Token::create($user_id);
 
+        $permissions = Permission::where('user_id',auth()->user()->id)
+            ->where('function','like',strtolower(extension()->name). '%')->pluck('function')->toArray();
+        for($i = 0 ;$i< count($permissions); $i++){
+            $permissions[$i] = explode('_',$permissions[$i])[1];
+        }
+        $permissions = str_replace('"', '*m*', json_encode($permissions));
         $command = "sudo runuser " . clean_score(extension()->id) .
             " -c 'timeout 30 /usr/bin/php -d display_errors=on $combinerFile $functions "
             . strtolower(extension()->name) .
-            " $viewName \"$server\" \"$extension\" \"$extensionDb\" \"$outputsJson\" \"$request\" \"$functionName\" \"$apiRoute\" \"$navigationRoute\" \"$token\" \"$extension_id\"'";
+            " $viewName \"$server\" \"$extension\" \"$extensionDb\" \"$outputsJson\" \"$request\" \"$functionName\" \"$apiRoute\" \"$navigationRoute\" \"$token\" \"$extension_id\" \"$permissions\"'";
 
         return $command;
     }
