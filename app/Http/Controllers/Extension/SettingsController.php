005e5896abd7196b7c11b1c6fdd0056bcd20c785
mertcelen <mcelen94@gmail.com>
2019-07-31 14:59:54 +0300

    New Extensions bugs fixed thanks to @mustafaakbel

--- app/Http/Controllers/Extension/SettingsController.php
+++ app/Http/Controllers/Extension/SettingsController.php
@@ -163,8 +163,8 @@ class SettingsController extends Controller
                             break;
                         case "views":
                             rename(env(
-                                'EXTENSIONS_PATH') . strtolower(extension()->name) . DIRECTORY_SEPARATOR . request('name_old') . '.blade.php',
-                                env('EXTENSIONS_PATH') . strtolower(extension()->name) . DIRECTORY_SEPARATOR . request('name') . '.blade.php');
+                                'EXTENSIONS_PATH') . strtolower(extension()->name) . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR. request('name_old') . '.blade.php',
+                                env('EXTENSIONS_PATH') . strtolower(extension()->name) . DIRECTORY_SEPARATOR .  "views" . DIRECTORY_SEPARATOR .request('name') . '.blade.php');
                             $values[$key]["scripts"] = request('scripts');
                             $values[$key]["name"] = request('name');
                             break;
