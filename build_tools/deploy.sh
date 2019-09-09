1ec87994f57bdb027b9b0d68f83e84426c45a150
mertcelen <mcelen94@gmail.com>
2019-09-09 14:51:48 +0300

    New Repository for Development Branch.

--- build_tools/deploy.sh
+++ build_tools/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -X POST -F "file=@/builds/liman/liman/liman-$1.deb" http://depo.lab:8080/api/files/liman
-curl -X POST http://depo.lab:8080/api/repos/liman/file/liman
-curl -X POST -H "Content-Type: application/json" -d '{"Name":"'$1'"}' http://depo.lab:8080/api/repos/liman/snapshots
+curl -X POST -F "file=@/builds/liman/liman/liman-$1.deb" http://depo.lab:8080/api/files/liman$2
+curl -X POST http://depo.lab:8080/api/repos/liman$2/file/liman
+curl -X POST -H "Content-Type: application/json" -d '{"Name":"'$1'"}' http://depo.lab:8080/api/repos/liman$2/snapshots
 curl -X PUT -H 'Content-Type: application/json' --data '{"Snapshots": [{"Component": "main", "Name": "'$1'"}],"GpgKey":"B29FB116187E58FC1FD88AB45B88DFDF4EC23DB3"}' http://depo.lab:8080/api/publish/:./ondokuz
