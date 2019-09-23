ce06ce7acfb51405af81043af4ee0a369112bf10
mertcelen <mcelen94@gmail.com>
2019-09-23 10:57:47 +0300

    Update warning to display only on admin account.

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -1,6 +1,6 @@
 <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
-            @if(auth()->check() && user()->status == "1")
+            @if(auth()->check() && user()->name == "Administrator")
                 <div class="alert alert-warning"
                      style="padding: 10px 25px 10px 15px !important; font-size: 12px; border-radius: 0px; margin-bottom: 0px; text-align: center; text-shadow: 3px 2px 3px rgba(255,255,255,.2); border: 0px; -webkit-box-shadow: 0px 0px 15px -2px rgba(0,0,0,0.75); -moz-box-shadow: 0px 0px 15px -2px rgba(0,0,0,0.75); box-shadow: 0px 0px 15px -2px rgba(0,0,0,0.75);">
                     {{__("Tam yetkili yönetici hesabı ile giriş yaptınız, sisteme zarar verebilirsiniz.")}}
