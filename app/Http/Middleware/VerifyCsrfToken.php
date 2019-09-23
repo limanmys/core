e4f47b69e978a46375ce1ad5f39d2bd1c3977c4c
mertcelen <mcelen94@gmail.com>
2019-09-23 22:02:12 +0300

    Fix for #138, #139, #140

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,7 +11,7 @@ class VerifyCsrfToken extends Middleware
      *
      * @var bool
      */
-    protected $addHttpCookie = true;
+    protected $addHttpCookie = false;
 
     /**
      * The URIs that should be excluded from CSRF verification.
