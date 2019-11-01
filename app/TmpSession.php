fdae2387ac6a24dffcce5480049df490448e2b08
Baran Sekin <baransekin@gmail.com>
2019-11-01 14:56:38 +0300

    Bug fix

--- app/TmpSession.php
+++ app/TmpSession.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class TmpSession extends Model
 {
     protected $fillable = ['session_id', 'key', 'value'];
+
+    public function getValueAttribute($value)
+    {
+        return json_decode($value);
+    }
 }
