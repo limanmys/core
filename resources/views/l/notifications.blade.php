5ad1d7d7790c122985dd5409c588c6e66d77ccfa
Baran Sekin <baransekin@gmail.com>
2019-11-07 20:49:12 +0300

    Bug fix

--- resources/views/l/notifications.blade.php
+++ resources/views/l/notifications.blade.php
@@ -1,35 +1,33 @@
-@if($notifications->count()>0)
-    <a class="nav-link" data-toggle="dropdown" href="#">
-        @if(!isset($systemNotification))
-            <i class="far fa-bell"></i>
-        @else
-            <i class="fas fa-cogs"></i>
-        @endif
-        <span class="badge badge-warning navbar-badge" id="{{isset($systemNotification) ? 'adminNotificationsCount' : 'userNotificationsCount'}}">{{$notifications->count()}}</span>
+<a class="nav-link" data-toggle="dropdown" href="#">
+    @if(!isset($systemNotification))
+        <i class="far fa-bell"></i>
+    @else
+        <i class="fas fa-cogs"></i>
+    @endif
+    <span class="badge badge-warning navbar-badge" id="{{isset($systemNotification) ? 'adminNotificationsCount' : 'userNotificationsCount'}}">{{$notifications->count()}}</span>
+</a>
+<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+    <span class="dropdown-item dropdown-header" onclick="@if(!isset($systemNotification)) readNotifications() @else readSystemNotifications() @endif">
+        {{__('Tümünü Okundu Olarak İşaretle')}}
+    </span>
+    @foreach ($notifications as $notification)
+    <div class="dropdown-divider"></div>
+        @switch($notification->type)
+            @case('error')
+            @case('health_problem')
+            @case('liman_update')
+            <a href="/bildirim/{{$notification->id}}" class="dropdown-item" style="color: #f56954;width: 100%">
+                {{$notification->title}}
+            </a>
+            @break
+            @default
+            <a href="/bildirim/{{$notification->id}}" class="dropdown-item" style="color: #00a65a;width: 100%">
+                {{$notification->title}}
+            </a>
+            @break
+        @endswitch
     </a>
-    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-        <span class="dropdown-item dropdown-header" onclick="@if(!isset($systemNotification)) readNotifications() @else readSystemNotifications() @endif">
-            {{__('Tümünü Okundu Olarak İşaretle')}}
-        </span>
-        @foreach ($notifications as $notification)
-        <div class="dropdown-divider"></div>
-            @switch($notification->type)
-                @case('error')
-                @case('health_problem')
-                @case('liman_update')
-                <a href="/bildirim/{{$notification->id}}" class="dropdown-item" style="color: #f56954;width: 100%">
-                    {{$notification->title}}
-                </a>
-                @break
-                @default
-                <a href="/bildirim/{{$notification->id}}" class="dropdown-item" style="color: #00a65a;width: 100%">
-                    {{$notification->title}}
-                </a>
-                @break
-            @endswitch
-        </a>
-        @endforeach
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item dropdown-footer" href="{{isset($systemNotification) ? route('all_system_notifications') : route('all_user_notifications')}}">{{__('Tümünü gör')}}</a>
-    </div>
-@endif
+    @endforeach
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item dropdown-footer" href="{{isset($systemNotification) ? route('all_system_notifications') : route('all_user_notifications')}}">{{__('Tümünü gör')}}</a>
+</div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
    <!-- Navbar -->
    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-        <!-- Left navbar links -->
+        <!-- Left navbar links 
         <ul class="navbar-nav">
           <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
@@ -11,9 +11,9 @@
           <li class="nav-item d-none d-sm-inline-block">
             <a href="#" class="nav-link">Contact</a>
           </li>
-        </ul>
+        </ul>-->
     
-        <!-- SEARCH FORM -->
+        <!-- SEARCH FORM 
         <form class="form-inline ml-3">
           <div class="input-group input-group-sm">
             <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
@@ -23,7 +23,7 @@
               </button>
             </div>
           </div>
-        </form>
+        </form>-->
     
         <!-- Right navbar links -->
         <ul class="navbar-nav ml-auto">
