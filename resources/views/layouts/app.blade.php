348fc2e259537613f364c9cdf83a9af1dda83971
Baran Sekin <baransekin@gmail.com>
2019-11-12 17:07:08 +0300

    Removed HAVELSAN brand

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,5 @@
             @include('layouts.header')
         @endauth
         @include('layouts.content')
-        <footer class="main-footer">
-            <div class="float-right d-none d-sm-block">
-                <b>{{ __("Versiyon") }}</b> {{ env('APP_VERSION') }}
-            </div>
-            <img src="{{ url('/images/havelsan_logo.png') }}" style="max-height: 30px;" alt="HAVELSAN A.Ş.">
-        </footer>
     </div>
 @stop
