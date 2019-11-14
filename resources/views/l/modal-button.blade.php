350179033760574dda7d5f13d044c713fa477104
Baran Sekin <baransekin@gmail.com>
2019-11-14 15:51:02 +0300

    Added icon to modal button

--- resources/views/l/modal-button.blade.php
+++ resources/views/l/modal-button.blade.php
@@ -1,5 +1,5 @@
 @if(isset($text,$class,$target_id))
 <button type="button" class="btn {{$class}}" data-toggle="modal" data-target="#{{$target_id}}">
-            {{__($text)}}
+    @if(isset($icon))<i class="{{$icon}}"></i> @endif{{__($text)}}
 </button>
 @endif
