1625f402a0275144c91dc25ba83300d47be09d49
Baran Sekin <baransekin@gmail.com>
2020-01-16 17:20:36 +0300

    Fixed 403 problem

---
+++ resources/views/errors/402.blade.php
@@ -0,0 +1,9 @@
+@if(request()->wantsJson() || $_SERVER['REMOTE_ADDR'] == "127.0.0.1")
+    @php(respond(__($exception->getMessage()),201))
+@else
+    @extends('layouts.app')
+
+@section('content')
+    <h1 class="ml-auto">{{__($exception->getMessage())}}</h1>
+@endsection
+@endif
