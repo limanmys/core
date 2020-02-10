45251cd5018071ccdbc0a2e141027e71da5a510c
mertcelen <mcelen94@gmail.com>
2020-02-10 11:12:05 +0300

    Added new release repository support.

--- build_tools/deploy.sh
+++ build_tools/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -X POST -F "file=@/builds/liman/liman/liman-$1.deb" http://depo.lab:8080/api/files/liman
-curl -X POST http://depo.lab:8080/api/repos/liman/file/liman
-curl -X POST -H "Content-Type: application/json" -d '{"Name":"'$1'"}' http://depo.lab:8080/api/repos/liman/snapshots
-curl -X PUT -H 'Content-Type: application/json' --data '{"Snapshots": [{"Component": "main", "Name": "'$1'"}],"GpgKey":"B29FB116187E58FC1FD88AB45B88DFDF4EC23DB3"}' http://depo.lab:8080/api/publish/:./ondokuz
+curl -X POST -F "file=@/builds/liman/liman/liman-$1.deb" http://$2:8080/api/files/$3
+curl -X POST http://$2:8080/api/repos/$3/file/liman
+curl -X POST -H "Content-Type: application/json" -d '{"Name":"'$1'"}' http://$2:8080/api/repos/$3/snapshots
+curl -X PUT -H 'Content-Type: application/json' --data '{"Snapshots": [{"Component": "main", "Name": "'$1'"}],"GpgKey":"B29FB116187E58FC1FD88AB45B88DFDF4EC23DB3"}' http://$2:8080/api/publish/:./ondokuz
