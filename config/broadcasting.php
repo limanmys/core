061e4e7ca5211d94aeab05d740595bdba8e56d14
Baran Sekin <baransekin@gmail.com>
2020-02-13 09:07:40 +0300

    Used app_key as pusher secret key

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -33,7 +33,7 @@ return [
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
+            'secret' => env('APP_KEY'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
