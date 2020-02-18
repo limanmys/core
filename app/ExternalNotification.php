34b5fab8dfe216cc5b067d167438d490a579b1d3
mertcelen <mcelen94@gmail.com>
2020-02-18 17:09:56 +0300

    External Notification Channel added (not enabled atm.)

---
+++ app/ExternalNotification.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExternalNotification extends Model
+{
+    use UsesUuid;
+
+    protected $fillable = [
+        "name", "last_used", "ip"
+    ];
+
+    protected $hidden = [
+        "token"
+    ];
+}
---
+++ database/migrations/2020_02_18_145445_create_external_notifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExternalNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('external_notifications', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('token');
+            $table->string('ip')->nullable();
+            $table->timestamp('last_used')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('external_notifications');
+    }
+}
