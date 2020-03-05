99119b2e9d8a9e10216770ebae8717dc243c43b2
Baran Sekin <baransekin@gmail.com>
2020-03-05 18:15:23 +0300

    Fixed ldap integration problems

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Adldap\Laravel\Facades\Adldap;
 use App\Http\Controllers\Controller;
 use App\LdapRestriction;
 use Carbon\Carbon;
--- app/Http/Controllers/Roles/RoleMappingController.php
+++ app/Http/Controllers/Roles/RoleMappingController.php
@@ -8,66 +8,59 @@ use Illuminate\Http\Request;
 use App\Role;
 use App\RoleMapping;
 use Illuminate\Support\Facades\Validator;
+use App\Classes\Connector\LdapConnection;
 
 class RoleMappingController extends Controller
 {
     public function fetchDomainGroups()
     {
-        $ldapUsername = request('ldapUsername');
-        $ldapPassword = request('ldapPassword');
-
-        $base_dn = config('ldap.ldap_base_dn');
-        $domain = config('ldap.ldap_domain');
-
         try{
-            $ldapConnection = ldap_connect("ldaps://" . config('ldap.ldap_host'));
-            ldap_set_option($ldapConnection, LDAP_OPT_PROTOCOL_VERSION, 3);
-            ldap_set_option($ldapConnection, LDAP_OPT_X_TLS_REQUIRE_CERT, LDAP_OPT_X_TLS_NEVER);
-            ldap_set_option($ldapConnection, LDAP_OPT_REFERRALS,0);
-            ldap_bind($ldapConnection, $ldapUsername."@".$domain, $ldapPassword);
+            $ldap = new LdapConnection(config('ldap.ldap_host'), request('ldapUsername'), request('ldapPassword'));
         }catch(\Exception $ex){
             return respond('LDAP Bağlantısı Kurulamadı', 201);
         }
+        $query = request('query');
 
-        $sr = ldap_search($ldapConnection, $base_dn, '(objectClass=group)');
-        $groups = ldap_get_entries($ldapConnection, $sr);
-        unset($groups['count']);
-        $data = [];
-        foreach($groups as $group){
-            $data[] = [
-                "dn" => $group['distinguishedname'][0],
-                "id" => $group['distinguishedname'][0]
-            ];
-        }
-        return respond($data);
+        list($size,$uglyGroups) = $ldap->search("(&(sAMAccountName=$query*)(sAMAccountType=268435456))",[
+            "attributeList" => [
+                "distinguishedname"
+            ]
+        ]);
+        unset($uglyGroups["count"]);
+    
+        $domainGroups = array_map(function($value){
+            return([
+                "id" => $value['distinguishedname'][0],
+                "text" => $value['distinguishedname'][0],
+            ]);
+        }, $uglyGroups);
+
+        return respond($domainGroups);
     }
 
     public function fetchDomainUsers()
     {
-        $ldapUsername = request('ldapUsername');
-        $ldapPassword = request('ldapPassword');
-
-        $base_dn = config('ldap.ldap_base_dn');
-        $domain = config('ldap.ldap_domain');
-
         try{
-            $ldapConnection = ldap_connect("ldaps://" . config('ldap.ldap_host'));
-            ldap_set_option($ldapConnection, LDAP_OPT_PROTOCOL_VERSION, 3);
-            ldap_set_option($ldapConnection, LDAP_OPT_X_TLS_REQUIRE_CERT, LDAP_OPT_X_TLS_NEVER);
-            ldap_set_option($ldapConnection, LDAP_OPT_REFERRALS,0);
-            ldap_bind($ldapConnection, $ldapUsername."@".$domain, $ldapPassword);
+            $ldap = new LdapConnection(config('ldap.ldap_host'), request('ldapUsername'), request('ldapPassword'));
         }catch(\Exception $ex){
             return respond('LDAP Bağlantısı Kurulamadı', 201);
         }
+        $query = request('query');
 
-        $sr = ldap_search($ldapConnection, $base_dn, '(objectClass=user)');
-        $groups = ldap_get_entries($ldapConnection, $sr);
-        unset($groups['count']);
-        $data = [];
-        foreach($groups as $group){
-            $data[] = $group['samaccountname'][0];
-        }
-        return respond($data);
+        list($size,$uglyUsers) = $ldap->search("(&(sAMAccountName=$query*)(sAMAccountType=805306368))",[
+            "attributeList" => [
+                "samaccountname"
+            ]
+        ]);
+        unset($uglyUsers["count"]);
+    
+        $domainUsers = array_map(function($value){
+            return([
+                "id" => $value['samaccountname'][0],
+                "text" => $value['samaccountname'][0],
+            ]);
+        }, $uglyUsers);
+        return respond($domainUsers);
     }
 
     public function addLdapRestriction()
