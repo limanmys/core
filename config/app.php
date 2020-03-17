0be3c4f577ac2cdf0a1a18e15370e337711857c7
mertcelen <mcelen94@gmail.com>
2020-03-17 13:30:13 +0300

    Remove dev requirement from production.

--- config/app.php
+++ config/app.php
@@ -161,8 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-
         /*
          * Package Service Providers...
          */
