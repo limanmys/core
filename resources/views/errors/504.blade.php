c41e46d87679a53fde00d95df869f1bf8e1cb99b
Baran Sekin <baransekin@gmail.com>
2020-05-07 15:31:54 +0300

    Fixed responsive design of error pages

--- resources/views/errors/504.blade.php
+++ resources/views/errors/504.blade.php
@@ -3,14 +3,14 @@
 @else
     @extends('layouts.app')
     @section('content')
-        <div class="error-page">
+        <div class="error-page mt-5">
             <h2 class="headline text-warning"><i class="fas fa-exclamation-triangle text-warning"></i></h2>
-            <div class="error-content" style="margin-top: 3rem;margin-left: 150px;">
+            <div class="error-content">
                 <h3>{{ __("Bir şeyler ters gitti.") }}</h3>
     
                 <p>
                     {{ $exception->getMessage() }}
-                    <a class="btn btn-primary mt-2" href="{{ URL::previous() }}">{{ __("Geri Dön") }}</a>
+                    <br><a class="btn btn-primary mt-2" href="{{ URL::previous() }}">{{ __("Geri Dön") }}</a>
                 </p>
             </div>
         </div>
