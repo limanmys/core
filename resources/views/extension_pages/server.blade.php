34c2d76f04c0be30003de9b59e7be7bce2e4ba83
mertcelen <mcelen94@gmail.com>
2020-05-12 12:34:26 +0300

    Support for javascript API helper.

--- resources/views/extension_pages/server.blade.php
+++ resources/views/extension_pages/server.blade.php
@@ -75,5 +75,14 @@
             
         }
     })
+
+    function API(target)
+    {
+        return "{{route('extension_server', [
+            "extension_id" => extension()->id,
+            "city" => server()->city,
+            "server_id" => server()->id,
+        ])}}/" + target;
+    }
 </script>
 @endsection
--- resources/views/extension_pages/server_restricted.blade.php
+++ resources/views/extension_pages/server_restricted.blade.php
@@ -6,4 +6,15 @@
 <div style="padding:30px;">
     {!!$view!!}
 </div>
+
+<script>
+    function API(target)
+    {
+        return "{{route('extension_server', [
+            "extension_id" => extension()->id,
+            "city" => server()->city,
+            "server_id" => server()->id,
+        ])}}/" + target;
+    }
+</script>
 @stop
