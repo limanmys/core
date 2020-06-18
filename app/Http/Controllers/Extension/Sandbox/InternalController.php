c059311a2b4fa61bcc03b4474108905d2c95e3ba
Mert ÇELEN <mcelen@havelsan.com.tr>
2020-06-18 13:15:45 +0300

    External api simplified.

--- app/Http/Controllers/Extension/Sandbox/InternalController.php
+++ app/Http/Controllers/Extension/Sandbox/InternalController.php
@@ -108,6 +108,33 @@ class InternalController extends Controller
 
     public function internalExtensions()
     {
+        $extension = Extension::where([
+            "name" => request("target_extension_name"),
+        ])->first();
+        if (!$extension) {
+            return false;
+        }
+
+        $server = Server::where(["id" => request("target_server_id")])->first();
+        if (!$server) {
+            return false;
+        }
+
+        $newRequestData = [
+            "extension_id" => $extension->id,
+            "extension" => $extension,
+            "server" => $server,
+            "server_id" => $server->id,
+        ];
+
+        $newRequestData = array_merge(
+            $newRequestData,
+            json_decode(request('extra_params'), true)
+        );
+        request()->merge($newRequestData);
+        $controller = new MainController();
+        $controller->initializeClass();
+        return $controller->API();
     }
 
     /**
@@ -314,11 +341,13 @@ class InternalController extends Controller
 
     public function sendLog()
     {
-        Log::channel('extension')->info(json_encode([
-            "log_id" => request('log_id'),
-            "message" => base64_encode(request('message')),
-            "title" => base64_encode(request('title'))
-        ]));
+        Log::channel('extension')->info(
+            json_encode([
+                "log_id" => request('log_id'),
+                "message" => base64_encode(request('message')),
+                "title" => base64_encode(request('title')),
+            ])
+        );
     }
 
     /**
