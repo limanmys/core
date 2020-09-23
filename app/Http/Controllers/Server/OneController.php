3bed8888d2fcb8bd62af8d001c147286b79b9551
mertcelen <mcelen94@gmail.com>
2020-09-23 18:39:44 +0300

    Tunnel debugs removed.

--- app/Http/Controllers/Server/OneController.php
+++ app/Http/Controllers/Server/OneController.php
@@ -317,7 +317,7 @@ class OneController extends Controller
             );
             $cpu = substr(
                 server()->run(
-                    "grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}'",
+                    "awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1); }' <(grep 'cpu ' /proc/stat) <(sleep 1;grep 'cpu ' /proc/stat)",
                     false
                 ),
                 0,
