7f818a876866c1d81af75eef79a5b4cdae73ad24
mertcelen <mcelen94@gmail.com>
2020-11-16 23:08:30 +0000

    Change default driver to pgsql

--- config/database.php
+++ config/database.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
