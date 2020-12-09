aa120368b2bfdd34a189a212540594fbbd3fe5ea
mertcelen <mcelen94@gmail.com>
2020-12-09 07:49:08 +0000

    Change user_id object type in migration.

--- database/migrations/2020_12_03_113317_create_user_monitors_table.php
+++ database/migrations/2020_12_03_113317_create_user_monitors_table.php
@@ -17,7 +17,7 @@ class CreateUserMonitorsTable extends Migration
             $table->uuid('id')->primary();
             $table->string('name');
             $table->uuid('server_monitor_id');
-            $table->uuid("user_id");
+            $table->string("user_id");
             $table
                 ->foreign("user_id")
                 ->references("id")
