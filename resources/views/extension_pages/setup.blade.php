44ce515bbaf681421d555fe8ece7d7c79b53e198
Doğukan Öksüz <me@dogukan.dev>
2021-08-03 13:24:46 +0300

    Translations added

--- resources/views/extension_pages/setup.blade.php
+++ resources/views/extension_pages/setup.blade.php
@@ -3,15 +3,10 @@
 @section('content')
 
 <ol class="breadcrumb">
-    <!--<li class="breadcrumb-item"><a href="{{route('home')}}">{{__("Ana Sayfa")}}</a></li>
-    <li class="breadcrumb-item"><a href="/l/{{extension()->id}}">{{extension()->display_name}} {{ __('Sunucuları') }}</a></li>
-    <li class="breadcrumb-item"><a href="/l/{{extension()->id}}/{{server()->city}}">{{cities(server()->city)}}</a></li>
-    <li class="breadcrumb-item"><a href='/l/{{extension()->id}}/{{server()->city}}/{{server()->id}}'>{{server()->name}}</a></li> 
-    <li class="breadcrumb-item active" aria-current="page">{{__("Eklenti Ayarları")}}</li>-->
     <li class="breadcrumb-item"><a href="{{route('home')}}">{{__("Ana Sayfa")}}</a></li>
     <li class="breadcrumb-item"><a href="{{route('server_one', server()->id)}}">{{ server()->name }}</a></li>
-    <li class="breadcrumb-item"><a href="/l/{{ server()->id }}/{{ extension()->id }}">{{__(extension()->display_name)}}</a></li>
-    <li class="breadcrumb-item active" aria-current="page">{{__(extension()->display_name)}} eklenti ayarları</li>
+    <li class="breadcrumb-item"><a href="/l/{{ server()->id }}/{{  }}/{{ extension()->id }}">{{__(extension()->display_name)}}</a></li>
+    <li class="breadcrumb-item active" aria-current="page">{{__(extension()->display_name)}}{{ __(' eklenti ayarları') }}</li>
 </ol>
 @include("extension_pages.setup_data")
 @endsection
