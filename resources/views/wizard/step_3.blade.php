2e2d344b2f499951f252279026a61a42c82d779f
Doğukan Öksüz <me@dogukan.dev>
2021-08-24 02:16:21 +0300

    Added changelogs

--- resources/views/wizard/step_3.blade.php
+++ resources/views/wizard/step_3.blade.php
@@ -4,6 +4,6 @@
                     <h1 class="text-2xl font-bold mb-10">
                         {{ __("Sürüm değişiklikleri") }}
                     </h1>
-
-                    Work in progress...
+                    
+                    <iframe src="/wizard/changelogs/{{ app()->getLocale() }}.html" frameborder="0" style="width: 100%; height: 400px; margin-bottom: 70px;"></iframe>
 @endsection
