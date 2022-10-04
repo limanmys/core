9c408f62d38f5522ed136136f57c9b1700f54153
dogukanoksuz <me@dogukan.dev>
2022-10-04 11:34:30 +0000

    feature: Caching on redis

--- config/cache.php
+++ config/cache.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'database'),
+    'default' => env('CACHE_DRIVER', 'redis'),
 
     /*
     |--------------------------------------------------------------------------
