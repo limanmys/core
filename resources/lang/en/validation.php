ffde45c9cea82557cbd02551a31dd3d6142dfd89
dogukanoksuz <me@dogukan.dev>
2022-10-31 14:16:28 +0000

    fix: Validation error translation

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -30,6 +30,7 @@ return [
     ],
     'boolean' => 'The :attribute field must be true or false.',
     'confirmed' => 'The :attribute confirmation does not match.',
+    'captcha' => 'You have entered wrong captcha.',
     'date' => 'The :attribute is not a valid date.',
     'date_equals' => 'The :attribute must be a date equal to :date.',
     'date_format' => 'The :attribute does not match the format :format.',
