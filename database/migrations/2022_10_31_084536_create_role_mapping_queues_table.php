e1b301c9f7284d2e1670e0990a70251df5692dcd
dogukanoksuz <me@dogukan.dev>
2022-10-31 07:30:15 +0000

    feature: Added non logged ldap user role assigning

---
+++ database/migrations/2022_10_31_084536_create_role_mapping_queues_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_mapping_queues', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('objectguid');
+            $table->uuid('role_id');
+        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_mapping_queues');
+    }
+};
