c5d902c3d5902ad56078b1a205576a9ce16a8c6c
dogukanoksuz <me@dogukan.dev>
2022-11-02 13:28:54 +0000

    feature: Dashboard new box styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,70 +2,72 @@
 
 @section('content')
     @include('errors')
-    <div class="callout callout-info shadow-sm">
-        <h5>{{__("Liman MYS'ye Hoşgeldiniz")}}</h5>
-        {{__("Kullanım rehberine ulaşmak için")}} <a href="https://docs.liman.dev/" target="_blank">https://docs.liman.dev/</a> {{__("adresini ziyaret edebilirsiniz.")}}
-    </div>
-    <div class="row" style="padding-top: 15px;">
-      <div class="col-md-3 col-sm-4 col-xs-12">
-          <div class="small-box shadow-sm bg-info">
-            <div class="inner">
-              <h3>{{$server_count}}</h3>
-              <p>{{__("Limandaki Sunucu Sayısı")}}</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-server"></i>
-            </div>
-            <a href="/sunucular" class="small-box-footer">
-              {{ __("Sunucuları listele") }} <i class="fas fa-arrow-circle-right"></i>
-            </a>
-          </div>
+    <div class="row">
+      <div class="col-md-12">
+      <div class="card homepage-widget">
+        <div class="card-body p-0">
+          <div class="row row-cols-xxl-5 row-cols-md-3 row-cols-1 g-0">
+                <div class="col">
+                    <div class="mt-3 mt-md-0 py-4 px-3">
+                        <h5 class="text-muted text-uppercase fs-13">{{ __('Sunucu Sayısı')}} <i class="fa-solid fa-server text-success fs-18 float-end align-middle"></i></h5>
+                        <div class="d-flex align-items-center">
+                            <div class="flex-shrink-0">
+                                <i class="ri-exchange-dollar-line display-6 text-muted"></i>
+                            </div>
+                            <div class="flex-grow-1 ms-3">
+                                <h2 class="mb-2"><span class="counter-value">{{ $server_count }}</span></h2>
+                                <a href="{{ route('servers') }}" class="text-muted text-uppercase" style="font-size: 11px">{{ __("Tüm Sunucuları Görüntüle") }} <i class="fa-solid fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- end col -->
+                <div class="col">
+                    <div class="mt-3 mt-md-0 py-4 px-3">
+                        <h5 class="text-muted text-uppercase fs-13">{{ __('Eklenti Sayısı')}} <i class="fa-solid fa-plug text-primary fs-18 float-end align-middle"></i></h5>
+                        <div class="d-flex align-items-center">
+                            <div class="flex-shrink-0">
+                                <i class="ri-exchange-dollar-line display-6 text-muted"></i>
+                            </div>
+                            <div class="flex-grow-1 ms-3">
+                                <h2 class="mb-2"><span class="counter-value">{{ $extension_count }}</span></h2>
+                                <a href="{{ route('settings') }}#extensions" class="text-muted text-uppercase" style="font-size: 11px">{{ __("Tüm Eklentileri Görüntüle") }} <i class="fa-solid fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- end col -->
+                <div class="col">
+                    <div class="mt-3 mt-md-0 py-4 px-3">
+                        <h5 class="text-muted text-uppercase fs-13">{{ __('Kullanıcı Sayısı')}} <i class="fa-solid fa-users text-navy fs-18 float-end align-middle"></i></h5>
+                        <div class="d-flex align-items-center">
+                            <div class="flex-shrink-0">
+                                <i class="ri-exchange-dollar-line display-6 text-muted"></i>
+                            </div>
+                            <div class="flex-grow-1 ms-3">
+                                <h2 class="mb-2"><span class="counter-value">{{ $user_count }}</span></h2>
+                                <a href="{{ route('settings') }}" class="text-muted text-uppercase" style="font-size: 11px">{{ __("Tüm Kullanıcıları Görüntüle") }} <i class="fa-solid fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- end col -->
+                <div class="col">
+                    <div class="mt-3 mt-md-0 py-4 px-3">
+                        <h5 class="text-muted text-uppercase fs-13">{{ __('Liman Versiyonu')}} <i class="fa-solid fa-cogs text-indigo fs-18 float-end align-middle"></i></h5>
+                        <div class="d-flex align-items-center">
+                            <div class="flex-shrink-0">
+                                <i class="ri-exchange-dollar-line display-6 text-muted"></i>
+                            </div>
+                            <div class="flex-grow-1 ms-3">
+                                <h2 class="mb-0"><span class="counter-value">{{ $version }}</span></h2>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- end col -->
+            </div><!-- end row -->
+          </div><!-- end card body -->
+        </div>
       </div>
-      
-      <div class="col-md-3 col-sm-4 col-xs-12">
-          <div class="small-box shadow-sm bg-info">
-            <div class="inner">
-              <h3>{{$extension_count}}</h3>
-              <p>{{__("Limandaki Eklenti Sayısı")}}</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-plug"></i>
-            </div>
-            <a href="/ayarlar#extensions" class="small-box-footer">
-              {{ __("Eklentileri yönet") }} <i class="fas fa-arrow-circle-right"></i>
-            </a>
-          </div>
-      </div>
-      <div class="col-md-3 col-sm-4 col-xs-12">
-          <div class="small-box shadow-sm bg-info">
-            <div class="inner">
-              <h3>{{$user_count}}</h3>
-              <p>{{__("Limandaki Kullanıcı Sayısı")}}</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-users"></i>
-            </div>
-            <a href="/ayarlar#users" class="small-box-footer">
-              {{ __("Kullanıcıları yönet") }} <i class="fas fa-arrow-circle-right"></i>
-            </a>
-          </div>
-      </div>
-      <div class="col-md-3 col-sm-4 col-xs-12">
-          <div class="small-box shadow-sm bg-info">
-            <div class="inner">
-              <h3>{{$version}}</h3>
-              <p>{{__("Liman Versiyonu")}}</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-cogs"></i>
-            </div>
-            <div class="small-box-footer" style="background-color: #17a2b8; border-radius: 5px">
-              &nbsp;
-            </div>
-          </div>
-      </div>
+
       @if(user()->isAdmin())
-        <script src="{{ asset('js/chart3.min.js') }}"></script>
         <div class="col-md-3 col-sm-4 col-xs-12">
             <div class="info-box shadow-sm loading chartbox" style="padding: 0; padding-top: 12px;">
               <div class="overlay" style="background: white">
@@ -118,7 +120,7 @@
           <div class="col-md-6 col-sm-12">
             <div class="card shadow-sm loading online-servers" style="height: 100%; min-height: 200px;">
                 <div class="card-header">
-                  <h3 class="card-title">{{ __("Sunucu Durumları") }}</h3>
+                  <h3 class="card-title" style="font-size: 15px">{{ __("Sunucu Durumları") }}</h3>
                 </div>
                 <div class="overlay">
                     <div class="spinner-border" role="status">
@@ -140,7 +142,7 @@
           <div class="col-md-6 col-sm-12">
             <div class="card shadow-sm loading market-widget" style="height: 100%; min-height: 200px;">
                 <div class="card-header p-0">
-                  <h3 class="card-title" style="padding: 12px; padding-left: 1.25rem">{{ __("Önerilen Eklentiler") }}</h3>
+                  <h3 class="card-title" style="padding: 12px; padding-left: 1.25rem; font-size: 15px">{{ __("Önerilen Eklentiler") }}</h3>
                   <div class="float-right">
                     <button style="margin: 5px" class="btn btn-sm btn-success" onclick="window.location.href='/market'"><i class="fas fa-shopping-cart mr-1"></i>{{ __("Eklenti Mağazası") }}</button>
                   </div>
@@ -218,7 +220,7 @@
                                 <li class="list-group-item">
                                 <a style="color:#222">
                                     <i class="fab mr-1"></i>
-                                    <span class="text-bold"></span>
+                                    <span style="font-weight: 600"></span>
                                 </a>
                                 <div class="float-right">
                                     <span class="text-xs"></span>  
