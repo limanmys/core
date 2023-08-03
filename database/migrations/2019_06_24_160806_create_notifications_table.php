18afb7d0f1656e82ce6b8ed098b65e94893b1f3d
dogukanoksuz <me@dogukan.dev>
2023-08-03 18:35:40 +0300

    fix: JWT secret and ui service

--- database/migrations/2019_06_24_160806_create_notifications_table.php
+++ database/migrations/2019_06_24_160806_create_notifications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOldNotificationsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -51,4 +51,4 @@ class CreateOldNotificationsTable extends Migration
     {
         Schema::dropIfExists('notifications');
     }
-}
+};
