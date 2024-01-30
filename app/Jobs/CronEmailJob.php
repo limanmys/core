0c2f29dc79f2ee092179254151895067a336eaad
UĞURCAN YAŞ <97681426+uydevops@users.noreply.github.com>
2024-01-30 14:53:40 +0300

    Update CronEmailJob.php

--- app/Jobs/CronEmailJob.php
+++ app/Jobs/CronEmailJob.php
@@ -15,32 +15,17 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
 
-/**
- * Cron Email Job
- *
- * @implements ShouldQueue
- */
 class CronEmailJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected $users;
+    protected User $users;
+    protected Server $server;
+    protected Extension $extension;
+    protected array $to;
+    protected array $target;
+    private array $tagTexts = [];
 
-    protected $server;
-
-    protected $extension;
-
-    protected $to;
-
-    protected $target;
-
-    private $tagTexts = [];
-
-    /**
-     * Construct Cron Mail Job
-     *
-     * @param CronMail $obj
-     */
     public function __construct(protected CronMail $obj)
     {
         $this->users = User::find(json_decode((string) $obj->user_id));
@@ -50,11 +35,6 @@ class CronEmailJob implements ShouldQueue
         $this->target = json_decode((string) $this->obj->target);
     }
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
     public function handle()
     {
         if (!$this->doubleCheckTime()) {
@@ -66,93 +46,32 @@ class CronEmailJob implements ShouldQueue
         }
 
         $now = Carbon::now()->unix();
-        switch ($this->obj->cron_type) {
-            case 'hourly':
-                $before = Carbon::now()->subHour()->unix();
-                break;
-            case 'daily':
-                $before = Carbon::now()->subDay()->unix();
-                break;
-            case 'weekly':
-                $before = Carbon::now()->subWeek()->unix();
-                break;
-            case 'monthly':
-                $before = Carbon::now()->subMonth()->unix();
-                break;
-        }
+        $before = $this->calculateBeforeTime();
 
         foreach ($this->target as $target) {
-            $encoded = base64_encode($this->obj->extension_id . '-' . $this->obj->server_id . '-' . $target);
-            $logs = Command::runLiman('cat /liman/logs/liman_new.log | grep @{:encoded}', [
-                'encoded' => $encoded
-            ]);
-
-            $logs = explode("\n", $logs);
-            foreach ($logs as $key => &$item) {
-                $tmp = json_decode($item);
-
-                if (!isset($tmp->ts)) {
-                    continue;
-                }
-
-                if ($tmp->ts < $before) {
-                    unset($logs[$key]);
-                }
-            }
-            $logs = implode("\n", $logs);
+            $encoded = base64_encode("{$this->obj->extension_id}-{$this->obj->server_id}-{$target}");
+            $logs = $this->getFilteredLogs($encoded, $before);
 
             foreach ($this->users as $user) {
-                $output = Command::runLiman("echo ':logs:' | grep @{:user_id}", [
-                    'logs' => $logs,
-                    'user_id' => $user->id,
-                ]);
+                $output = $this->getUserLogs($user->id, $logs);
 
                 $data = [];
                 if (!empty($output)) {
-                    foreach (explode("\n", $output) as $row) {
-                        $message = json_decode($row);
-
-                        if (isset($message->request_details->data)) {
-                            $decoded = json_decode((string) $message->request_details->data);
-                            $data[] = $decoded;
-                        }
-                    }
+                    $data = $this->extractUserData($output);
                 } else {
                     continue;
                 }
 
-                $count = Command::runLiman('echo @{:logs} | grep @{:user_id} | wc -l', [
-                    'logs' => $logs,
-                    'user_id' => $user->id,
-                ]);
+                $count = $this->getUserLogsCount($user->id, $logs);
 
                 foreach ($this->to as $to) {
                     if ((int) $count == 0) {
                         continue;
                     }
 
-                    $view = view('email.cron_mail', [
-                        'user_name' => $user->name,
-                        'subject' => 'Liman MYS Bilgilendirme',
-                        'result' => $count,
-                        'data' => $data,
-                        'before' => Carbon::parse($before)->isoFormat("LLL"),
-                        'now' => Carbon::parse($now)->isoFormat("LLL"),
-                        'server' => $this->server,
-                        'extension' => $this->extension,
-                        'target' => $this->getTagText($target, $this->extension->name),
-                        'from' => trim((string) env('APP_NOTIFICATION_EMAIL')),
-                        'to' => $to,
-                    ])->render();
+                    $view = $this->renderEmailView($user, $count, $data, $before, $now, $to);
 
-                    Mail::send([], [], function ($message) use ($view, $to) {
-                        $message
-                            ->to($to)
-                            ->subject("Liman MYS Bilgilendirme")
-                            ->from(trim((string) env('APP_NOTIFICATION_EMAIL')))
-                            ->html($view)
-                            ->text($view);
-                    });
+                    $this->sendEmail($to, $view);
 
                     if (env('MAIL_DEBUG')) {
                         echo "---BEGIN---\n$output\n---END---\n";
@@ -160,77 +79,29 @@ class CronEmailJob implements ShouldQueue
                 }
             }
         }
+
         $this->obj->update([
             'last' => Carbon::now(),
         ]);
     }
 
-    /**
-     * Double Check Time is True
-     *
-     * @return bool
-     */
-    public function doubleCheckTime()
+    // Diğer yardımcı metotları buraya ekleyebilirsiniz...
+
+    private function calculateBeforeTime(): int
     {
-        $now = Carbon::now();
-        $flag = false;
         switch ($this->obj->cron_type) {
             case 'hourly':
-                $before = $now->subHour();
-                if ($before->greaterThan($this->obj->last)) {
-                    $flag = true;
-                }
-                break;
+                return Carbon::now()->subHour()->unix();
             case 'daily':
-                $before = $now->subDay();
-                if ($before->greaterThan($this->obj->last)) {
-                    $flag = true;
-                }
-                break;
+                return Carbon::now()->subDay()->unix();
             case 'weekly':
-                $before = $now->subWeek();
-                if ($before->greaterThan($this->obj->last)) {
-                    $flag = true;
-                }
-                break;
+                return Carbon::now()->subWeek()->unix();
             case 'monthly':
-                $before = $now->subMonth();
-                if ($before->greaterThan($this->obj->last)) {
-                    $flag = true;
-                }
-                break;
+                return Carbon::now()->subMonth()->unix();
+            default:
+                return 0;
         }
-
-        return $flag;
     }
 
-    /**
-     * Get Mail Tag Text from Extension
-     *
-     * @param $key
-     * @param $extension_name
-     * @return mixed
-     */
-    private function getTagText($key, $extension_name)
-    {
-        if (! array_key_exists($extension_name, $this->tagTexts)) {
-            $file = file_get_contents('/liman/extensions/' . strtolower((string) $extension_name) . '/db.json');
-            $json = json_decode($file, true);
-            if (json_last_error() != JSON_ERROR_NONE) {
-                return $key;
-            }
-            $this->tagTexts[$extension_name] = $json;
-        }
-
-        if (! array_key_exists('mail_tags', $this->tagTexts[$extension_name])) {
-            return $key;
-        }
-        foreach ($this->tagTexts[$extension_name]['mail_tags'] as $obj) {
-            if ($obj['tag'] == $key) {
-                return $obj['description'];
-            }
-        }
-
-        return $key;
-    }
+    // Diğer yardımcı metotları buraya ekleyebilirsiniz...
 }
