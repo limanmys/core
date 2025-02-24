674e9f6dcfcde25f926f1803180f19288d54db5f
dogukanoksuz <me@dogukan.dev>
2025-02-24 12:25:13 +0000

    feat-wip: PHP 8.4 and Laravel 12 prep

--- config/database.php
+++ config/database.php
@@ -31,13 +31,6 @@ return [
     */
 
     'connections' => [
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => '/liman/database/liman.sqlite',
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
